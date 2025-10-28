076149085646e51d1f4f5a601830ed068bcbb230
JeffM20501 <kimmuna440@gmail.com>
2025-10-28 18:09:37 +0300

    compeleted lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Home from './Home'
 import NavBar from './NavBar'
 import About from './About'
+import {username, city, image} from '../data/user'
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city} />
+      <About image={image} />
     </div>
   );
 }
